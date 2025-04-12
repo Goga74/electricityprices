b1bdbedb663354846dfba31aceb76bbe66d6a126
Igor Zamiatin <capra.lanigera@gmail.com>
2025-04-13 00:38:18 +0300

    Run with docker

--- Dockerfile
+++ Dockerfile
@@ -10,5 +10,8 @@ COPY target/electricity-prices-1.0-SNAPSHOT.jar app.jar
 # Открываем порт, на котором работает Spring Boot (8074)
 EXPOSE 8074
 
+# Устанавливаем активный профиль для Docker
+ENV SPRING_PROFILES_ACTIVE=docker
+
 # Команда для запуска приложения
 CMD ["java", "-jar", "app.jar"]
--- src/main/resources/sql/create.sql
+++ src/main/resources/sql/create.sql
@@ -1,5 +1,19 @@
-CREATE TABLE price_data (
-                            id BIGINT AUTO_INCREMENT PRIMARY KEY,
-                            date DATE NOT NULL UNIQUE,
-                            json_data JSON NOT NULL
-) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci;
+CREATE DATABASE IF NOT EXISTS `elprices` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci */ /*!80016 DEFAULT ENCRYPTION='N' */;
+USE `elprices`;
+
+-- Дамп структуры для таблица elprices.price_data
+CREATE TABLE IF NOT EXISTS `price_data` (
+                                            `id` bigint NOT NULL AUTO_INCREMENT,
+                                            `date` date NOT NULL,
+                                            `json_data` json NOT NULL,
+                                            PRIMARY KEY (`id`),
+    UNIQUE KEY `date` (`date`)
+    ) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+
+CREATE USER 'eluser'@'%' IDENTIFIED BY 'eluser';
+GRANT USAGE ON *.* TO 'eluser'@'%';
+GRANT SELECT, DELETE, UPDATE, INSERT, LOCK TABLES, EXECUTE, SHOW VIEW, CREATE TEMPORARY TABLES, TRIGGER  ON `elprices`.* TO 'eluser'@'%';
+FLUSH PRIVILEGES;
+SHOW GRANTS FOR 'eluser'@'%';
+SHOW CREATE USER 'eluser'@'%';
